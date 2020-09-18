9f87d642567420418970dfa24d1fb7b235508b0d
Ahmad Shela <ahmedsh717@gmail.com>
2020-09-18 18:09:08 +0300

    challenge10

--- __test__/llzip.test.js
+++ __test__/llzip.test.js
@@ -1,19 +1,19 @@
 'use strict';
 const LinkedList = require('../challenges/Data-Structure/linked_list/linked_list');
-const ll_Zip = require('../challenges/ll_zip/ll_zip');
+require('../challenges/ll_zip/ll_zip');
 describe('llZip',()=>{
-    it('llZip return zipped list',()=>{
-        let firstList = new LinkedList();
-        let secList = new LinkedList();
-        firstList.append(1);
-        firstList.append(3);
-        firstList.append(5);
-        secList.append(2);
-        secList.append(4);
-        secList.append(6);
-        console.log('firstList',firstList);
-        console.log('secList',secList);
-        // expect(ll_Zip(firstList,secList).head).toEqual(1)
-        // console.log(ll_Zip(firstList,secList));;
-    })
+  it('llZip return zipped list',()=>{
+    let firstList = new LinkedList();
+    let secList = new LinkedList();
+    firstList.append(1);
+    firstList.append(3);
+    firstList.append(5);
+    secList.append(2);
+    secList.append(4);
+    secList.append(6);
+    console.log('firstList',firstList);
+    console.log('secList',secList);
+    // expect(ll_Zip(firstList,secList).head).toEqual(1)
+    // console.log(ll_Zip(firstList,secList));;
+  });
 });
