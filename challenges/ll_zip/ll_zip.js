6c6297e98dc841c52ad2c9ccac532d7ba0b9fecd
Ahmad Shela <ahmedsh717@gmail.com>
2020-09-18 18:09:58 +0300

    challenge10

--- challenges/ll_zip/ll_zip.js
+++ challenges/ll_zip/ll_zip.js
@@ -1,24 +1,24 @@
 'use strict';
 
-const list = require('../Data-Structure/linked_list/linked_list.js')
+const list = require('../Data-Structure/linked_list/linked_list.js');
 // const ll = new list();
 
 const ll_Zip = function (list1,list2) {
-    console.log('>>>>>>>>> LL',list1);
-    var zippedList = new list();
-    var firstLL = list1;
-    var secLL = list2;
-    var firstListNode = firstLL.head;
-    var secListNode = secLL.head;
+  console.log('>>>>>>>>> LL',list1);
+  var zippedList = new list();
+  var firstLL = list1;
+  var secLL = list2;
+  var firstListNode = firstLL.head;
+  var secListNode = secLL.head;
 
-    while(firstListNode || secListNode){
-        zippedList.append(firstListNode.value);
-        zippedList.append(secListNode.value);
-        firstListNode = firstListNode.next
-        secListNode = secListNode.next
-    }
-    return zippedList;
+  while(firstListNode || secListNode){
+    zippedList.append(firstListNode.value);
+    zippedList.append(secListNode.value);
+    firstListNode = firstListNode.next;
+    secListNode = secListNode.next;
+  }
+  return zippedList;
     
-}
+};
 
 module.exports = ll_Zip;
