3ae5eae80b42397abd8c4976d0d00152b2bf03c6
Ahmad Shela <ahmedsh717@gmail.com>
2020-09-26 21:33:11 +0300

    fixDocs

--- __test__/queueWithStacks.test.js
+++ __test__/queueWithStacks.test.js
@@ -2,16 +2,16 @@
 const PseudoQueue = require('../challenges/queueWithStacks/queueWithStacks');
 
 describe('PseudoQueue',()=>{
-    it('can successfully enqueue a value into an empty PseudoQueue', () => {
-        const pq = new PseudoQueue();
-        pq.enqueue(5);
-        expect(pq.stack1.peek().value).toEqual(5);
-      });
-      it('can successfully dequeue a value from PseudoQueue in the correct order', () => {
-        const pq = new PseudoQueue();
-        pq.enqueue('salmon');
-        pq.enqueue('mackerel');
-        pq.enqueue('yellowtail');
-        expect(pq.dequeue()).toBe('salmon');
-      });
-})
+  it('can successfully enqueue a value into an empty PseudoQueue', () => {
+    const pq = new PseudoQueue();
+    pq.enqueue(5);
+    expect(pq.stack1.peek().value).toEqual(5);
+  });
+  it('can successfully dequeue a value from PseudoQueue in the correct order', () => {
+    const pq = new PseudoQueue();
+    pq.enqueue('salmon');
+    pq.enqueue('mackerel');
+    pq.enqueue('yellowtail');
+    expect(pq.dequeue()).toBe('salmon');
+  });
+});
