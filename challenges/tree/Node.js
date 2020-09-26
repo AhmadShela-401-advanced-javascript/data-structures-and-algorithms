792f8bbf5c153c626aa2f443415a63f35069d3ff
Ahmad Shela <ahmedsh717@gmail.com>
2020-09-26 21:33:59 +0300

    fixDocs

--- challenges/tree/Node.js
+++ challenges/tree/Node.js
@@ -1,11 +1,11 @@
 'use strict';
 
 class Node {
-    constructor(val){
-        this.left = null;
-        this.value = val;
-        this.right = null;
-    }
+  constructor(val){
+    this.left = null;
+    this.value = val;
+    this.right = null;
+  }
 }
 
 module.exports = Node;
